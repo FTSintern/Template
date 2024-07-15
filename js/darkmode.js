6657d1e4d9997c81403058381d09d4f27405b9ca
FTS-Dev3 <131987160+FTS-Dev3@users.noreply.github.com>
2024-07-15 22:22:09 +0500

    initial commit

---
+++ js/darkmode.js
@@ -0,0 +1,45 @@
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+
+
+    // Example: Change color of text elements with different classes
+    const changeColorElements = document.querySelectorAll('.contenttext, .contentheading, .line2, .aboutparagraph, .review , .reviewer , .card-titles');
+    const changeColorElementsDark = document.querySelectorAll('.mode, .hero-main, .hero-paragraph, .appointmentheading, .btn-block, .custom-info-text, .card-texts');
+    const changeBgElements = document.querySelectorAll('.testimonial-card, .appointment-from , .custom-info-card , .feature-item , .service-card');
+
+    changeColorElements.forEach(element => {
+        if (document.body.classList.contains('dark-mode')) {
+            element.style.setProperty('color', '#ffffff', 'important'); // Set text color to white in dark mode
+        } else {
+            // Revert to default styles
+            element.style.color = ''; // Reset text color
+        }
+    });
+
+    changeColorElementsDark.forEach(element => {
+        if (document.body.classList.contains('dark-mode')) {
+            element.style.setProperty('color', '#ffffff', 'important'); // Set text color to white in dark mode
+        } else {
+            // Revert to default styles
+            element.style.color = ''; // Reset text color
+        }
+    });
+
+    changeBgElements.forEach(element => {
+        if (document.body.classList.contains('dark-mode')) {
+            element.style.setProperty('background-color', '#1a1a1a', 'important'); // Set background color to dark
+            element.style.setProperty('border', '1px solid #ffffff', 'important'); // Add white border
+        } else {
+            // Revert to default styles
+            element.style.backgroundColor = ''; // Reset background color
+            element.style.border = ''; // Reset border
+        }
+    });
+}
+
+document.getElementById('themeToggle').addEventListener('click', function (event) {
+    event.preventDefault();
+    toggleDarkMode();
+});
+
+
---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+document.getElementById('menuToggle').addEventListener('click', function () {
+    var navbar = document.getElementById('navbarNav');
+    if (navbar.classList.contains('show')) {
+        navbar.classList.remove('show');
+    } else {
+        navbar.classList.add('show');
+    }
+});
